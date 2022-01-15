0f9ea8467f0fcbea6b3dabc113c36b4969852ce6
OceanShape <oceanshape700@gmail.com>
2022-01-15 14:38:23 +0900

    feat: Create DetailViewController
    - add close()

---
+++ [KDC]BountyList/DetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DetailViewController.swift
+//  [KDC]BountyList
+//
+//  Created by 허태양 on 2022/01/15.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
